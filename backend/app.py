969c077c89b031d3c45ee31c642ab700d97c6246
vuongnp <vuong.np040119991@gmail.com>
2021-06-14 20:55:19 +0700

    full backend

--- backend/app.py
+++ backend/app.py
@@ -5,16 +5,15 @@ from flask_cors import CORS
 import pymongo
 import os
 from dotenv import load_dotenv
+import numpy as np
+import json
 
-from statictic.getStatistic import countByYear, countByGenre
+from statictic.getStatistic import countByYear, countByGenre, avgRating, results
 
 load_dotenv()
 
 app = Flask(__name__)
 
-# app.config['MONGODB_SETTINGS'] = {
-#     'host': 'mongodb+srv://vuongnp:1234566@vuongcluster1.gl4g4.mongodb.net/film'
-# }
 app.config['MONGODB_SETTINGS'] = {
     'host': os.getenv("MONGODB_URI")
 }
@@ -24,6 +23,8 @@ db = client.film
 CORS(app)
 
 initialize_db(app)
+
+# model = pickle.load(open('./models/KMeans.pkl', 'rb'))
 
 # A welcome message to test our server
 @app.route('/')
@@ -46,37 +47,48 @@ def get_movies():
 @app.route('/search', methods=['GET'])
 def search_movies():
     text = request.args.get("text")
+    page = request.args.get("page")
+    limit = request.args.get("limit")
     movies = General.objects.search_text(text).order_by('$text_score')
-    res = {'data': movies}
+    length=len(movies)
+    if page:
+        start = int(limit)*(int(page)-1)
+        end = start+int(limit)
+        movies = movies[start:end]
+    res = {'data': movies,'length':length}
     return res
 
 @app.route('/category', methods=['GET'])
 def get_movies_by_catogory():
     text = request.args.get("text")
+    page = request.args.get("page")
+    limit = request.args.get("limit")
     movies=''
     if text=='imdb':
-        movies = General.objects(rating__exists=True).order_by('-rating')
+        movies = General.objects(rating__exists=True).order_by('-rating')[:100]
     else:
         movies = General.objects(genres__icontains=text).order_by('-rating')
-    res = {'data': movies}
+    length=len(movies)
+    if page:
+        start = int(limit)*(int(page)-1)
+        end = start+int(limit)
+        movies = movies[start:end]
+    res = {'data': movies,'length':length}
     return res
 
 @app.route('/statistic', methods=['GET'])
 def get_statistic():
     general = list(db.general.find({},{'_id':0}))
     label1,data1 = countByYear(general)
-    label2,data2 = countByGenre(general)
+    label2,count,label3,avg,label4,countSources = results(general)
+    # label2,data2 = countByGenre(general)
     statisticYear={'labels':label1, 'data':data1}
-    statisticGenres = {'labels':label2, 'data':data2}
-    res = {'statisticYear': statisticYear, 'statisticGenres':statisticGenres}
+    statisticGenres = {'labels':label2, 'data':count}
+    statisticAvgRating = {'labels':label3, 'data':avg}
+    statisticNumUrls = {'labels':label4, 'data':countSources}
+    # statisticGenres = {'labels':label2, 'data':data2}
+    res = {'statisticYear': statisticYear, 'statisticGenres':statisticGenres, 'statisticAvgRating':statisticAvgRating, 'statisticNumUrls':statisticNumUrls}
     return res
-
-@app.route('/stat', methods=['GET'])
-def stat_movie():
-    text = request.args.get("text")
-    movies = Movie.objects.search_text(text).order_by('$text_score')
-    return Response(movies.to_json(), mimetype="application/json", status=200)
-
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
--- backend/statictic/getStatistic.py
+++ backend/statictic/getStatistic.py
@@ -26,4 +26,63 @@ def countByGenre(general):
     for gen in labels:
         data.append(int(genres[gen]))
     return (labels,data)
+def avgRating(general):
+    general = pd.DataFrame(general)
+    labels=['Drama','Comedy','Action','Adventure','Horror','Thriller','Documentary','Crime','Romance','Mystery','Fantasy','Sci-Fi','Animation','Family']
+    rating={'Drama':0,'Comedy':0,'Action':0,'Adventure':0,'Horror':0,'Thriller':0,'Documentary':0,'Crime':0,'Romance':0,'Mystery':0,'Fantasy':0,'Sci-Fi':0,'Animation':0,'Family':0}
+    count={'Drama':0,'Comedy':0,'Action':0,'Adventure':0,'Horror':0,'Thriller':0,'Documentary':0,'Crime':0,'Romance':0,'Mystery':0,'Fantasy':0,'Sci-Fi':0,'Animation':0,'Family':0}
+    for i,row in general.iterrows():
+        for gen in labels:
+            if general.genres.iloc[i] is not None:
+                if gen in list(general.genres.iloc[i]):
+                    if general.rating.iloc[i] is not None and pd.notnull(general.rating.iloc[i]):
+                        rating[gen] = rating[gen]+int(general.rating.iloc[i])
+                        count[gen] = count[gen]+1
+    avg=[]
+    label=[]
+    couting=[]
+    for key in labels:
+        label.append(key)
+        avg.append(rating[key]/count[key])
+        couting.append(count[key])
+    
+    return (label,count,avg)
+def getCountLinks(general):
+    counts=[]
+    count1 = 0
+    count2 = 0
+    count3=0
+    count4=0
+    count5=0
+    count6=0
+    count7=0
+    count8=0
+    for ind in general.index:
+        if len(general['urls'].loc[ind])==1:
+            count1+=1
+        elif len(general['urls'].loc[ind])==2:
+            count2+=1
+        elif len(general['urls'].loc[ind])==3:
+            count3+=1
+        elif len(general['urls'].loc[ind])==4:
+            count4+=1
+        elif len(general['urls'].loc[ind])==5:
+            count5+=1
+        elif len(general['urls'].loc[ind])==6:
+            count6+=1
+        elif len(general['urls'].loc[ind])==7:
+            count7+=1
+        elif len(general['urls'].loc[ind])==8:
+            count8+=1
+    counts=[count1]+[count2]+[count3]+[count4]+[count5]+[count6]+[count7]+[count8]
+    labels = ['1 sources','2 sources','3 sources','4 sources','5 sources','6 sources', '7 sources', '8 sources']
+    return (labels,counts)
+
+def results(general):
+    labels=['Drama', 'Comedy', 'Action','Crime', 'Adventure', 'Thriller', 'Romance','Horror', 'Mystery', 'Fantasy', 'Sci-Fi', 'Animation', 'Family', 'Documentary']
+    count=[7212, 5061, 2961, 2517, 2181, 2057,1945, 1637, 1367, 1033, 982, 834, 805, 142]
+    avg=[6.254575707154742, 5.994467496542185, 5.856805133400878, 6.033929390187987, 5.281612706169823, 5.627126883811376, 7.211267605633803, 6.185935637663886, 5.989717223650386, 6.01609363569861, 5.833494675701839, 5.689409368635438, 6.571942446043166, 5.885714285714286]
+    sources=['1 sources','2 sources','3 sources','4 sources','5 sources','6 sources', '7 sources', '8 sources']
+    countSources=[31933, 4006, 1188, 282, 14, 0, 0, 0]
+    return (labels,count,labels,avg,sources,countSources)
 
